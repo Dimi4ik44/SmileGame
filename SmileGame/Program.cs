c2d177f9c94191e201650b1cc546df005e468e6b
Dmutro <dimi4ik44@gmail.com>
2021-11-03 18:03:17 +0300

    Add project files.

---
+++ SmileGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmileGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
